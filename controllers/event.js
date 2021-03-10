661ce45b7f5a69af0233a6c8cf7508d36d1a8e49
busayomi <bsayomi.olayiwola8@gmail.com>
2021-03-10 06:52:00 -0600

    user-create-event

--- controllers/event.js
+++ controllers/event.js
@@ -23,25 +23,46 @@ const show = (req, res) => {
     });
 };
 
-const create = (req, res) => {
+const create = async (req, res) => {
     // Purpose: Create one example by adding body to DB, and return
     console.log('=====> Inside POST /events');
     console.log('=====> req.body');
     console.log(req.body); // object used for creating new example
     console.log(req.user)
-    db.Event.create({
+
+    const createEvent = await db.Event.create({
         title: req.body.title,
         time: req.body.time,
         comment: req.body.comment,
-       
-    }, (err, savedEvent) => {
-        if (err) {
-            console.log("Error in event:" + err)
-        } else {
-            res.json(savedEvent)
-        }
+
     })
+    console.log(createEvent)
+    if (!createEvent) {
+        console.log("Error in event")
+    }
+
+    
+
+    const foundUser = await db.User.findByIdAndUpdate(req.user.id,
+         {$addToSet: { events: createEvent._id }},
+        {safe: true}
+    ) 
+
+    if (!foundUser) {
+        console.log('Error in event#show');
+    }
+    // await foundUser.events.push(createEvent._id)
+    console.log("===> event created succesfully")
+    res.json(createEvent)
 }
+
+
+
+
+
+
+
+
 const update = (req, res) => {
     // Purpose: Update one example in the DB, and return
     console.log('=====> Inside PUT /events/:id');
@@ -61,11 +82,11 @@ const destroy = (req, res) => {
     console.log('=====> Inside DELETE /events/:id');
     console.log('=====> req.params');
     console.log(req.params); // object used for finding example by id
-    
+
     db.Event.findByIdAndDelete(req.params.id, (err, deletedEvent) => {
         if (err) console.log('Error in event#destroy:', err);
-          res.sendStatus(200);
-          console.log(deletedEvent);
+        res.sendStatus(200);
+        console.log(deletedEvent);
     });
 };
 
@@ -77,16 +98,36 @@ module.exports = {
     destroy,
 };
 
-///////////////////////////////////////////////////////////////////////////////////////////
-// DON'T USE YET - Not tested
-// const query = (req, res) => {
-//     // Purpose: Fetch one example via query from DB and return
-//     console.log('=====> Inside "query" POST /examples/query');
-//     console.log('=====> req.query');
-//     console.log(req.query); // object using for doing a query search on an example
+// const create = async (req, res) => {
+//     // Purpose: Create one example by adding body to DB, and return
+//     console.log('=====> Inside POST /events');
+//     console.log('=====> req.body');
+//     console.log(req.body); // object used for creating new example
+//     console.log(req.user)
+//  const createEvent =  await db.Event.create({
+//         title: req.body.title,
+//         time: req.body.time,
+//         comment: req.body.comment,
 
-//     db.Example.find(req.query, (err, foundExample) => {
-//         if (err) console.log('Error in example#query:', err);
-//         res.json(foundExample);
+//     }), (err, savedEvent) => {
+//         console.log(savedEvent)
+//         if (err) {
+//             console.log("Error in event:" + err)
+//         } else {
+//             // const findUserById = async (req, res) => {
+//                 const foundUser = db.User.findOne({ _id: req.user._id }, (err, foundUser) => {
+//                     if (err) {
+//                         console.log('Error in event#show:', err);
+//                     } else {
+//                         foundUser.events.push(savedEvent._id)
+//                         console.log("===> event created succesfully")
+//                     }
+//                     res.json(savedEvent)
+//                 })
+//             // }
+//         }
 //     });
 // }
+
+
+
